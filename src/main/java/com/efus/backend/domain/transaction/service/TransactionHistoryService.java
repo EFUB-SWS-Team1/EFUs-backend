7622d3ae8e54ed28b5eb1078f47398612e74bce8
Zin Wai Yan <zinwaiyan2226@gmail.com>
2026-07-28 17:00:53 +0900

    [FEAT] term 연결후 주석 해제

--- src/main/java/com/efus/backend/domain/transaction/service/TransactionHistoryService.java
+++ src/main/java/com/efus/backend/domain/transaction/service/TransactionHistoryService.java
@@ -2,6 +2,7 @@ package com.efus.backend.domain.transaction.service;
 
 import com.efus.backend.domain.transaction.dto.response.TransactionHistoryListResponse;
 import com.efus.backend.domain.transaction.dto.response.TransactionHistoryResponse;
+import com.efus.backend.domain.transaction.entity.Transaction;
 import com.efus.backend.domain.transaction.repository.TransactionHistoryRepository;
 import com.efus.backend.global.exception.CustomException;
 import com.efus.backend.global.exception.ErrorCode;
@@ -21,8 +22,7 @@ public class TransactionHistoryService {
     private static final int DEFAULT_SIZE = 20;
     private final TransactionHistoryRepository transactionHistoryRepository;
 
-    // TODO: transaction 단건 조회 메서드 병합 후 주석 해제
-    // private final TransactionQueryService transactionQueryService;
+     private final TransactionQueryService transactionQueryService;
 
     // TODO: member QueryService 병합 후 주석 해제
     // private final MemberQueryService memberQueryService;
@@ -37,9 +37,8 @@ public class TransactionHistoryService {
 
         validatePageRequest(pageNumber, pageSize);
 
-        // TODO: term 연관관계 + TransactionQueryService 병합 후 주석 해제 (transactionId로 거래를 먼저 조회하고, 거래가 속한 termId를 가져와야 함)
-        // Transaction transaction = transactionQueryService.getTransaction(transactionId);
-        // Long termId = transaction.getTerm().getId();
+         Transaction transaction = transactionQueryService.getTransaction(transactionId);
+         Long termId = transaction.getTerm().getId();
         // TODO: member QueryService 병합 후 주석 해제 (조회 API이므로 STAFF/ACTIVE 검증은 하지 않고, 해당 기수 구성원 여부만 검증)
         // memberQueryService.validateTermMember(termId);
 
--- src/main/java/com/efus/backend/domain/transaction/service/TransactionQueryService.java
+++ src/main/java/com/efus/backend/domain/transaction/service/TransactionQueryService.java
@@ -18,20 +18,18 @@ public class TransactionQueryService {
          return transactionRepository.findById(transactionId)
                  .orElseThrow(() -> new CustomException(ErrorCode.TRANSACTION_NOT_FOUND));
      }
-    // TODO: funding,term 연관관계 + FundingQueryService,MemberQueryService,TermQueryService 병합 후 주석 해제
-//    public Transaction getTransactionInTerm(Long termId, Long transactionId) {
-//        return transactionRepository.findByIdAndTerm_Id(transactionId, termId)
-//                .orElseThrow(() -> new CustomException(ErrorCode.TRANSACTION_NOT_FOUND));
-//    }
+    public Transaction getTransactionInTerm(Long termId, Long transactionId) {
+        return transactionRepository.findByIdAndTerm_Id(transactionId, termId)
+                .orElseThrow(() -> new CustomException(ErrorCode.TRANSACTION_NOT_FOUND));
+    }
 
-    // TODO: funding,term 연관관계 + FundingQueryService,MemberQueryService,TermQueryService 병합 후 주석 해제
-//    public Transaction getActiveTransactionInTerm(Long termId, Long transactionId) {
-//        Transaction transaction = getTransactionInTerm(termId, transactionId);
-//
-//        if (transaction.isDeleted()) {
-//            throw new CustomException(ErrorCode.TRANSACTION_ALREADY_DELETED);
-//        }
-//
-//        return transaction;
-//    }
+    public Transaction getActiveTransactionInTerm(Long termId, Long transactionId) {
+        Transaction transaction = getTransactionInTerm(termId, transactionId);
+
+        if (transaction.isDeleted()) {
+            throw new CustomException(ErrorCode.TRANSACTION_ALREADY_DELETED);
+        }
+
+        return transaction;
+    }
 }
