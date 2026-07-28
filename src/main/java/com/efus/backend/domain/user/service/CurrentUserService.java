3092efd2d39263d8aaa01bff35868a21840a9bc0
Lee Hyunkyung <michelle3hg@gmail.com>
2026-07-28 09:45:01 +0900

    feat: User 도메인 접근 규칙 구현

--- src/main/java/com/efus/backend/domain/user/service/CurrentUserService.java
+++ src/main/java/com/efus/backend/domain/user/service/CurrentUserService.java
@@ -5,6 +5,7 @@ import com.efus.backend.domain.user.repository.UserRepository;
 import com.efus.backend.global.exception.CustomException;
 import com.efus.backend.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,14 +15,15 @@ import org.springframework.transaction.annotation.Transactional;
 public class CurrentUserService {
 
     private final UserRepository userRepository;
-// TODO: 도메인 간 접근 규칙(로그인 사용자)
 
-//    public User getCurrentUser() {
-//        // 1. SecurityContext에서 현재 인증된 사용자의 ID 추출 (JwtFilter에서 세팅했다고 가정)
-//        String kakaoId = SecurityContextHolder.getContext().getAuthentication().getName();
-//
-//        // 2. DB에서 유저 객체를 찾아 반환
-//        return userRepository.findByKakaoId(Long.valueOf(kakaoId))
-//                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
-//    }
+    public User getCurrentUser() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (principal instanceof Long userId) {
+            return userRepository.findById(userId)
+                    .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+        }
+
+        throw new CustomException(ErrorCode.UNAUTHORIZED);
+    }
 }
